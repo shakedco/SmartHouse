26540088470ba18bfd45dd81b9100ddd1422417b
abedakhras <abed16021994@gmail.com>
2019-02-21 07:09:58 +0200

    server code

--- arduinoServerCode/sketch_nov25a.ino
+++ arduinoServerCode/sketch_nov25a.ino
@@ -1,168 +1,80 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-*********/
-
-// Load Wi-Fi library
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
 
-// Replace with your network credentials
-const char* ssid     = "BeHappy";
-const char* password = "k1234565";
+ESP8266WebServer server(80);
+ 
+const char* ssid = "arc";
+const char* password =  "abed1234";
+           //0,1,2,3,4,5, 6, 7, 8,9 ,10
+int state[11]={0,0,0,0,0,0, 0, 0, 0, 0, 0};
+int arr[11]  ={0,1,2,3,4,5,12,13,14,15,16};
 
-// Set web server port number to 80
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String header;
-
-// Auxiliar variables to store the current output state
-String output5State = "off";
-String output4State = "off";
-String output3State = "off";
-
-// Assign output variables to GPIO pins
-const int output5 = 5;
-const int output4 = 4;
-const int output3 = 3;
-
-void setup() {
-  Serial.begin(115200);
-  // Initialize the output variables as outputs
-  pinMode(output5, OUTPUT);
-  pinMode(output4, OUTPUT);
-  pinMode(output3, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output5, LOW);
-  digitalWrite(output4, LOW);
-  digitalWrite(output3, LOW);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+void initPinMode(){
+         
+  for(int i=0; i<11 ; i++){
+    pinMode(arr[i],OUTPUT);
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
-}
-
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+  for(int i=0; i<11 ; i++){
+    digitalWrite(arr[i],LOW);
+  }    
   
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0) {
-              Serial.println("GPIO 5 on");
-              output5State = "on";
-              digitalWrite(output5, HIGH);
-            } else if (header.indexOf("GET /5/off") >= 0) {
-              Serial.println("GPIO 5 off");
-              output5State = "off";
-              digitalWrite(output5, LOW);
-            } else if (header.indexOf("GET /4/on") >= 0) {
-              Serial.println("GPIO 4 on");
-              output4State = "on";
-              digitalWrite(output4, HIGH);
-            } else if (header.indexOf("GET /4/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output4State = "off";
-              digitalWrite(output4, LOW);
-            }else if (header.indexOf("GET /3/on") >= 0) {
-              Serial.println("GPIO 3 on");
-              output3State = "on";
-              digitalWrite(output3, HIGH);
-            } else if (header.indexOf("GET /3/off") >= 0) {
-              Serial.println("GPIO 4 off");
-              output3State = "off";
-              digitalWrite(output3, LOW);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #77878A;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP8266 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 5  
-            client.println("<p>GPIO 5 - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button       
-            if (output5State=="off") {
-              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 4 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button       
-            if (output4State=="off") {
-              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
-
-            // Display current state, and ON/OFF buttons for GPIO 4  
-            client.println("<p>GPIO 3 - State " + output3State + "</p>");
-            // If the output3State is off, it displays the ON button       
-            if (output3State=="off") {
-              client.println("<p><a href=\"/3/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/3/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            
-            client.println("</body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
+}
+void setup() {
+    initPinMode();
+    Serial.begin(115200);
+    WiFi.begin(ssid, password);  //Connect to the WiFi network
+ 
+    while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
+ 
+        delay(500);
+        Serial.println("Waiting to connect...");
+ 
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+ 
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());  //Print the local IP
+ 
+    server.on("/body", handleBody); //Associate the handler function to the path
+ 
+    server.begin(); //Start the server
+    Serial.println("Server listening");
+ 
+}
+ 
+void loop() {
+ 
+    server.handleClient(); //Handling of incoming requests
+ 
+}
+ 
+void handleBody() { //Handler for the body path
+ 
+      if (server.hasArg("plain")== false){ //Check if body received
+ 
+            server.send(200, "text/plain", "Body not received");
+            return;
+ 
+      }
+ 
+      String message = "";
+             message += server.arg("plain");
+  
+      StaticJsonBuffer<200> jsonBuffer;
+      JsonObject& root = jsonBuffer.parseObject(message);
+      int ledId = root["ledId"];
+      
+      if(state[ledId]==0){
+        Serial.println(ledId);
+        Serial.println(arr[ledId]);
+        Serial.println(state[ledId]);
+        state[ledId]=1;
+        digitalWrite(arr[ledId],HIGH);
+      }else{
+        state[ledId]=0;
+        digitalWrite(arr[ledId],LOW);
+      }
+
+      server.send(200, "text/plin", message);
+      Serial.println(message);
 }
