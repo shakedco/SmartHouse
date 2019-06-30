6625b86a9b81cb15dd240c25a32df14eb598d252
abed akhras <abed16021994@gmail.com>
2019-06-30 16:44:05 +0300

    Delete Old File

--- SmartHouseLV/components/RoomCard/RoomButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View,Text ,StyleSheet ,Navigator, Image,Alert , Button,TouchableHighlight} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-
-export class RoomButton extends React.Component {
-    
-}
